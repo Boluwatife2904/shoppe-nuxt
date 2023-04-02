a29f7a896ed8defefc6b42003c631f9909433882
Boluwatife2904 <sanvicola2000@gmail.com>
2023-04-02 08:15:54 +0100

    chore: implement error 404 page

---
+++ error.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+interface Props {
+	error: Object;
+}
+
+defineProps<Props>();
+
+const goBackHome = () => {
+	navigateTo({ name: "index" }, { replace: true });
+};
+</script>
+
+<template>
+	<main class="default-layout">
+		<TheHeader />
+		<div class="error-content">
+			<h3 class="error-content__title heading-1">{{ error.statusCode }} Error</h3>
+			<p class="error-content__message heading-3">
+				<template v-if="error.statusCode === 404">
+					This page not found; <br />
+					back to home and start again
+				</template>
+				<template v-else> An unexpected error occurred and that's why you're seeing this page. Go back home and start again.</template>
+			</p>
+			<BaseButton variant="outline-black" size="medium" text="HOMEPAGE" @click="goBackHome" />
+		</div>
+		<TheFooter />
+	</main>
+</template>
+
+<style lang="scss" scoped>
+main {
+	min-height: 100vh;
+}
+
+.error-content {
+	width: 100%;
+	text-align: center;
+	margin-top: 10rem;
+
+	@media screen and (min-width: 600px) {
+		margin-top: 27.2rem;
+	}
+
+	&__title {
+		color: var(--black-color);
+		margin-bottom: 1.2rem;
+
+		@media screen and (min-width: 600px) {
+			margin-bottom: 2.4rem;
+		}
+	}
+
+	&__message {
+		color: var(--dark-gray);
+		margin-bottom: 2.8rem;
+
+		@media screen and (min-width: 600px) {
+			margin-bottom: 6.4rem;
+		}
+	}
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<main class="main-layout">
+	<main class="default-layout">
 		<TheHeader />
 		<slot />
 		<TheFooter />
@@ -9,18 +9,4 @@
 	</main>
 </template>
 
-<style lang="scss">
-.main-layout {
-	max-width: 124.8rem;
-	margin: 0 auto;
-	padding: 1.5rem 1.6rem 2.4rem;
-
-	@media screen and (min-width: 600px) {
-		padding: 4.8 2rem 9rem;
-	}
-
-	@media screen and (min-width: 992px) {
-		padding: 4.8rem 3.2rem 9rem;
-	}
-}
-</style>
+<style lang="scss"></style>
