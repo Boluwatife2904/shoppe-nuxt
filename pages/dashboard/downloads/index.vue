a94cb066acdd183cd080be67ae1b6a1639acad6b
Boluwatife2904 <sanvicola2000@gmail.com>
2023-04-11 00:07:05 -1000

    work on the empty state for the downloads page

--- pages/dashboard/downloads/index.vue
+++ pages/dashboard/downloads/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 definePageMeta({
-    layout: "dashboard"
-})
+	layout: "dashboard",
+});
 </script>
 
 <template>
-	<div>Downloads</div>
+	<div class="orders">
+		<DashboardPageHeader title="Downloads" />
+		<div class="orders__content">
+			<BaseAlert :show-icon="false" text="No downloads available yet" link-text="BROWSE PRODUCTS" link-address="/shop" />
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
