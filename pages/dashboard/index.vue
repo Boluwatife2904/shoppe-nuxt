3114769eeb17dc9223833294bb2b25d9487e95f4
Boluwatife2904 <sanvicola2000@gmail.com>
2023-04-15 16:44:46 -1000

    chore: add the dashboard main content

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 definePageMeta({
-    layout: "dashboard"
-})
+	layout: "dashboard",
+});
 </script>
 
 <template>
-	<div>Dashboard Page</div>
+	<div class="dashboard-home">
+		<DashboardPageHeader title="Dashboard" />
+		<div class="dashboard-home__content flex flex-column">
+			<p class="heading-5">Hello Vitatheme (Not Vitatheme? <span class="accent-text">Log out</span>)</p>
+			<p class="heading-5">From your account dashboard you can view your 
+                <span class="accent-text" @click="navigateTo('/dashboard/orders')">recent orders</span>, 
+                manage your <span class="accent-text" @click="navigateTo('/dashboard/addresses')">shipping and billing addresses</span>, 
+                and edit your <span class="accent-text" @click="navigateTo('/dashboard/account')">password and account details</span>.
+            </p>
+		</div>
+	</div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.dashboard-home {
+
+    &__content {
+        gap: 2rem;
+    }
+
+    span {
+        cursor: pointer;
+    }
+}
+</style>
