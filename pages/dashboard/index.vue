380a67463f56afbf5aedf425cd791785ba82e46b
Boluwatife2904 <sanvicola2000@gmail.com>
2023-04-16 23:32:52 -1000

    chore: add logout

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -2,31 +2,28 @@
 definePageMeta({
 	layout: "dashboard",
 });
+
+const logout = () => {};
 </script>
 
 <template>
 	<div class="dashboard-home">
 		<DashboardPageHeader title="Dashboard" />
 		<div class="dashboard-home__content flex flex-column">
-			<p class="heading-5">Hello Vitatheme (Not Vitatheme? <span class="accent-text">Log out</span>)</p>
-			<p class="heading-5">From your account dashboard you can view your 
-                <span class="accent-text" @click="navigateTo('/dashboard/orders')">recent orders</span>, 
-                manage your <span class="accent-text" @click="navigateTo('/dashboard/addresses')">shipping and billing addresses</span>, 
-                and edit your <span class="accent-text" @click="navigateTo('/dashboard/account')">password and account details</span>.
-            </p>
+			<p class="heading-5">Hello Vitatheme (Not Vitatheme? <span class="accent-text" @click="logout">Log out</span>)</p>
+			<p class="heading-5">From your account dashboard you can view your <span class="accent-text" @click="navigateTo('/dashboard/orders')">recent orders</span>, manage your <span class="accent-text" @click="navigateTo('/dashboard/addresses')">shipping and billing addresses</span>, and edit your <span class="accent-text" @click="navigateTo('/dashboard/account')">password and account details</span>.</p>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 .dashboard-home {
+	&__content {
+		gap: 2rem;
+	}
 
-    &__content {
-        gap: 2rem;
-    }
-
-    span {
-        cursor: pointer;
-    }
+	span {
+		cursor: pointer;
+	}
 }
 </style>
