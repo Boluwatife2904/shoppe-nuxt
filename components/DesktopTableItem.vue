dae94b4a93e40e229f267c89bf9ddb07801c492a
Boluwatife2904 <sanvicola2000@gmail.com>
2023-05-04 08:20:23 +0100

    chore: add the table for the downloads page

--- components/DesktopTableItem.vue
+++ components/DesktopTableItem.vue
@@ -6,34 +6,4 @@
 	</div>
 </template>
 
-<style lang="scss">
-.table__item {
-	width: 100%;
-	gap: 1rem;
-	border-bottom: 1px solid var(--gray);
-	padding-bottom: 2.4rem;
-	margin-bottom: 2.4rem;
-
-	> span {
-		&:nth-child(1) {
-			width: 28%;
-		}
-
-		&:nth-child(2) {
-			width: 18%;
-		}
-
-		&:nth-child(3) {
-			width: 18%;
-		}
-
-		&:nth-child(4) {
-			width: 18%;
-		}
-
-		&:nth-child(5) {
-			width: 18%;
-		}
-	}
-}
-</style>
+<style lang="scss" scoped></style>
--- pages/dashboard/downloads/index.vue
+++ pages/dashboard/downloads/index.vue
@@ -2,12 +2,38 @@
 definePageMeta({
 	layout: "dashboard",
 });
+
+const headers = ["order number", "date", "status", "total", "actions"];
+
+const orders = [
+	{ orderNumber: "7643980998990", date: "October 8,2021", status: "Delivered", total: "$ 105" },
+	{ orderNumber: "943980998990", date: "October 8,2021", status: "Processing", total: "$ 100" },
+	{ orderNumber: "879980998990", date: "October 8,2020", status: "Delivered", total: "$ 65" },
+];
+
+const viewOrder = (orderNumber: string) => {};
+const downloadOrder = (orderNumber: string) => {};
 </script>
 
 <template>
 	<div class="downloads">
 		<DashboardPageHeader title="Downloads" />
 		<div class="downloads__content">
+			<DesktopTable :headers="headers">
+				<template #body>
+					<DesktopTableItem v-for="order in orders" :key="order.orderNumber">
+						<span>{{ order.orderNumber }}</span>
+						<span>{{ order.date }}</span>
+						<span>{{ order.status }}</span>
+						<span>{{ order.total }}</span>
+						<span>
+							<button class="button body-large accent-text" @click="viewOrder(order.orderNumber)">View Order</button> |
+							<button class="button body-large accent-text" @click="downloadOrder(order.orderNumber)">Download</button>
+						</span>
+					</DesktopTableItem>
+				</template>
+			</DesktopTable>
+
 			<BaseAlert :show-icon="false" text="No downloads available yet" link-text="BROWSE PRODUCTS" link-address="/shop" />
 		</div>
 	</div>
